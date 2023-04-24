d96b3c7ac685cdbddf27d715b270eb7a46623543
henintsoa454 <henintsoa404@gmail.com>
2023-04-24 19:10:05 +0430

    sprint3

--- framework/FrontServlet.java
+++ framework/FrontServlet.java
@@ -1,7 +1,10 @@
 package etu1923.framework.servlet;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.servlet.ServletException;
@@ -20,6 +23,51 @@ public class FrontServlet extends HttpServlet{
 
 	public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
 		this.mappingUrls = mappingUrls;
+	}
+	
+	public static ArrayList<Class<?>> checkClasses(File directory, String packageName) throws ClassNotFoundException {
+        ArrayList<Class<?>> classes = new ArrayList<>();
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                classes.addAll(checkClasses(file, packageName + "." + file.getName()));
+            } else if (file.getName().endsWith(".class")) {
+                String className = packageName + "." + file.getName().substring(0, file.getName().length() - 6);
+                Class<?> clazz = Class.forName(className);
+                classes.add(clazz);
+            }
+        }
+        return classes;
+    }
+	
+	@Override
+	public void init() throws ServletException {
+		 File f = null;
+	        try{
+	            f = new File("../webapps/Framework/WEB-INF/classes/etu1923");
+	            ArrayList<Class<?>> classes = FrontServlet.checkClasses(f,"etu1923");
+	            for(int i = 0;i<classes.size();i++){
+	                Class<?> classe = classes.get(i);
+	                Method[] methods = classe.getDeclaredMethods();
+	                for (Method method : methods) {
+	                    if (method.isAnnotationPresent(etu1923.framework.annotation.AppRoute.class)) {	
+	                        String url = method.getAnnotation(etu1923.framework.annotation.AppRoute.class).url();
+	                        Mapping newmap = new Mapping();
+	                        System.out.println(classe.getName());
+	                        System.out.println(method.getName());
+	                        newmap.setClassName(classe.getName());
+	                        newmap.setMethod(method.getName());
+	                        this.getMappingUrls().put(url,newmap);
+	                    }
+	                }
+	            } 
+	        }
+	        catch(Exception e){
+	            e.printStackTrace();
+	        }
 	}
 
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {		
