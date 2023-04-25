b1406bef1a281094634d992621794b4bcae554e9
henintsoa454 <henintsoa404@gmail.com>
2023-04-25 17:14:09 +0430

    sprint 5

---
+++ framework/ModelView.java
@@ -0,0 +1,22 @@
+package etu1923.framework;
+
+public class ModelView {
+	String url;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	public ModelView() {
+		
+	}
+	
+	public ModelView(String url) {
+		this.setUrl(url);
+	}
+}
