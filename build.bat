7498049be22a62548c57e243b111244388ae5151
henintsoa454 <henintsoa404@gmail.com>
2023-04-25 06:22:22 +0430

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,6 @@
+jar cf framework/framework.jar framework/bin/etu1923
+cp framework/framework.jar test/WEB-INF/lib
+jar cf test/test.war test/
+cp test/test C:\Program Files\Apache Software Foundation\Tomcat 8.5_Tomcat8.5\webapps
+
+
