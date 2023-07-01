014e8984a173c32f3fd8b38482e50843849679bb
henintsoa454 <henintsoa404@gmail.com>
2023-07-01 11:18:41 +0430

    Update ModelView.java

--- framework/ModelView.java
+++ framework/ModelView.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class ModelView {
 	String url;
 	HashMap<String,Object> data;
+	HashMap<String, Object> session;
 	
 	public String getUrl() {
 		return url;
@@ -21,19 +22,30 @@ public class ModelView {
 	public void setData(HashMap<String, Object> data) {
 		this.data = data;
 	}
+	
+	public HashMap<String, Object> getSession() {
+		return session;
+	}
+
+	public void setSession(HashMap<String, Object> session) {
+		this.session = session;
+	}
 
 	public ModelView() {
 		this.setData(new HashMap<>());
+		this.setSession(new HashMap<>());
 	}
 	
 	public ModelView(String url) {
 		this.setUrl(url);
 		this.setData(new HashMap<>());
+		this.setSession(new HashMap<>());
 	}
 	
 	public ModelView(String url,HashMap<String,Object> data) {
 		this.setUrl(url);
 		this.setData(new HashMap<>());
+		this.setSession(new HashMap<>());
 		this.setData(data);
 	}
 	
@@ -41,5 +53,11 @@ public class ModelView {
 		if(!this.getData().containsKey(key)) {
 			this.getData().put(key, value);
 		}
+	}
+	
+	public void addSession(String key,Object value) {
+		// if(!this.getSession().containsKey(key)) {
+			this.getSession().put(key, value);
+		// }
 	}
 }
